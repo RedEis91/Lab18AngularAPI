00a4f584a591410499656fafd2a78b137fe507a6
RedEis91 <imitationkrabs91@gmail.com>
2017-02-28 20:36:34 -0500

    added footer image, changed some names

--- module.js
+++ module.js
@@ -14,11 +14,20 @@ app.config(function($routeProvider, $locationProvider){
 });
 
 
-app.directive("logoBogo", function() {
+app.directive("logo", function() {
     return {
         restrict: "EA",
         replace: true,
-        template: "<img src='banner-angularjs.jpg' alt='Tony Hawk' height=25% width=25%>"
+        template: "<img src='banner-angularjs.jpg' alt='logo' height=25% width=25%>"
+    }
+});
+  
+
+app.directive("footerLogo", function() {
+    return {
+        restrict: "EA",
+        replace: true,
+        template: "<img src='ATeam1.jpg' alt='footerLogo' height=50% width=50% >"
     }
 });
   
