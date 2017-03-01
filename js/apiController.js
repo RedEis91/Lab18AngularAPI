48967aacab9dbd6932d121afc15346c5c0200acd
Christina L <clee04@baker.edu>
2017-02-28 22:31:46 -0500

    another one

--- js/apiController.js
+++ js/apiController.js
@@ -1,4 +1,4 @@
 var app = angular.module("lab18Module");
-app.controller('apiCtrl', function ($scope, $location, apiFactory) {
+app.controller('apiCtrl', function ($scope, $location, apiFactory, infoFactory) {
 $scope.robotz = apiFactory.responseBody();
 $scope.
