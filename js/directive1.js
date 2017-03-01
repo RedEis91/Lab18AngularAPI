622e0974ea1acf1e972ed9543caf90b78f9351fa
priyankashaji <priyankashaji@gmail.com>
2017-02-28 21:47:50 -0500

    put every thing in folders

--- js/directive1.js
+++ js/directive1.js
@@ -3,8 +3,8 @@ var app = angular.module(‘myModule’, []);
 app.directive('helloWorld', function(){
   return{
     restrict: "CAME",
-    template: "<img src="banner-angularjs.jpg" alt="angular banner">",
+    template: '<img src="img/banner-angularjs.jpg" alt="angular banner">',
     replace: false
   };
-  
+
 });
--- js/module.js
+++ js/module.js
@@ -18,16 +18,16 @@ app.directive("logo", function() {
     return {
         restrict: "EA",
         replace: true,
-        template: "<img src='banner-angularjs.jpg' alt='logo' height=25% width=25%>"
+        template: "<img src='img/banner-angularjs.jpg' alt='logo' height=25% width=25%>"
     }
 });
-  
+
 
 app.directive("footerLogo", function() {
     return {
         restrict: "EA",
         replace: true,
-        template: "<img src='ATeam1.jpg' alt='footerLogo' height=50% width=50% >"
+        template: "<img src='img/ATeam1.jpg' alt='footerLogo' height=50% width=50% >"
     }
 });
   
