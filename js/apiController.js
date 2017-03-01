79e62048dbe7db8e9864b81e7238b666a712d290
Christina L <clee04@baker.edu>
2017-02-28 22:28:22 -0500

    new controller and minor updates to index and factory

---
+++ js/apiController.js
@@ -0,0 +1,4 @@
+var app = angular.module("lab18Module");
+app.controller('apiCtrl', function ($scope, $location, apiFactory) {
+$scope.robotz = apiFactory.responseBody();
+$scope.
--- js/apiFactory.js
+++ js/apiFactory.js
@@ -1,14 +1,17 @@
 var app = angular.module("lab18Module");
-app.factory('apiFactory', function(){
-  
-$.get({
-  url: 'https://robohash.p.mashape.com/index.php?text=fizzbuzz',
-  headers: {
-    "X-Mashape-Key": "lOInYNzHQ2mshMjc4ErlgCx8FjcNp1IwEUHjsnGVFb2yp1SelN",
-    "Accept": "application/json"
-  }
-}, function(responseBody) {
-  log(responseBody);
-});
+app.factory('apiFactory', function() {
+        var robotz;
 
-});
+        return {
+            $.get({
+                url: 'https://robohash.p.mashape.com/index.php?text=fizzbuzz',
+                headers: {
+                    "X-Mashape-Key": "lOInYNzHQ2mshMjc4ErlgCx8FjcNp1IwEUHjsnGVFb2yp1SelN",
+                    "Accept": "application/json"
+                }
+            }, function(responseBody) {
+                log(responseBody);
+            });
+
+        });
+}
