3806fdb952f4694e33bcc2be738300d387ff7885
Christina L <clee04@baker.edu>
2017-03-02 15:02:57 -0500

    erased ngAnimate

--- js/module.js
+++ js/module.js
@@ -1,4 +1,4 @@
-var app = angular.module("lab18Module", ['ngRoute'], ['ngAnimate']);
+var app = angular.module("lab18Module", ['ngRoute']);
 app.config(function($routeProvider, $locationProvider){
     $routeProvider
     .when('/ngFizzBuzz', {
